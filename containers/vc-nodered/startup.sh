3700ff0bcf4004e65de8985c2574495abb9ffe6c
Franck Besnard <besn0847@gmail.com>
2017-11-18 13:54:30 +0100

    adding flows.json to nodered

--- containers/vc-nodered/startup.sh
+++ containers/vc-nodered/startup.sh
@@ -3,10 +3,11 @@
 if [ ! -f /conf/.bootstrapped_nodered ]
 then
 	cp /bootstrap/settings.js /conf/
+	cp /bootstrap/flows.json /conf/
 	echo 1 > /conf/.bootstrapped_nodered
 fi
 
 while(true)
 do
-	sudo -u vc npm start -- --userDir /data --settings /conf/settings.js
+	sudo -u vc npm start -- --userDir /data --settings /conf/settings.js /conf/flows.json
 done
